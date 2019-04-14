0a7e66a38257c132019715959c179d75e97cf21a
Mohamed Taman <mohamed.taman@gmail.com>
2019-04-14 20:36:38 +0200

    Step 18.5, MCMXCVI = 1996

--- RomanConverter/src/test/java/rs/com/tm/siriusxi/tdd/roman/RomanConverterTest.java
+++ RomanConverter/src/test/java/rs/com/tm/siriusxi/tdd/roman/RomanConverterTest.java
@@ -39,51 +39,68 @@ class RomanConverterTest {
 
     @Test
     void convertI() {
-        assertEquals(1, convertRomanToArabicNumber("I"), "Roman I should equal to 1");
+        assertEquals(1, convertRomanToArabicNumber("I"),
+                "Roman I should equal to 1");
     }
 
     @Test
     void convertII() {
-        assertEquals(2, convertRomanToArabicNumber("II"), "Roman II should equal to 2");
+        assertEquals(2, convertRomanToArabicNumber("II"),
+                "Roman II should equal to 2");
     }
 
     @Test
     void convertIII() {
-        assertEquals(3, convertRomanToArabicNumber("III"), "Roman III should equal to 3");
+        assertEquals(3, convertRomanToArabicNumber("III"),
+                "Roman III should equal to 3");
     }
 
     @Test
     void convertV() {
-        assertEquals(5, convertRomanToArabicNumber("V"), "Roman V should equal to 5");
+        assertEquals(5, convertRomanToArabicNumber("V"),
+                "Roman V should equal to 5");
     }
 
     @Test
     void convertVI() {
-        assertEquals(6, convertRomanToArabicNumber("VI"), "Roman VI should equal to 6");
+        assertEquals(6, convertRomanToArabicNumber("VI"),
+                "Roman VI should equal to 6");
     }
 
     @Test
     void convertIV() {
-        assertEquals(4, convertRomanToArabicNumber("IV"), "Roman IV should equal to 4");
+        assertEquals(4, convertRomanToArabicNumber("IV"),
+                "Roman IV should equal to 4");
     }
 
     @Test
     void convertX() {
-        assertEquals(10, convertRomanToArabicNumber("X"), "Roman X should equal to 10");
+        assertEquals(10, convertRomanToArabicNumber("X"),
+                "Roman X should equal to 10");
     }
 
     @Test
     void convertIX() {
-        assertEquals(9, convertRomanToArabicNumber("IX"), "Roman IX should equal to 9");
+        assertEquals(9, convertRomanToArabicNumber("IX"),
+                "Roman IX should equal to 9");
     }
 
     @Test
     void convertXXXVI() {
-        assertEquals(36, convertRomanToArabicNumber("XXXVI"), "Roman XXXVI should equal to 36");
+        assertEquals(36, convertRomanToArabicNumber("XXXVI"),
+                "Roman XXXVI should equal to 36");
     }
 
     @Test
     void convertMMXII() {
-        assertEquals(2012, convertRomanToArabicNumber("MMXII"), "Roman MMXII should equal to 2012");
+        assertEquals(2012, convertRomanToArabicNumber("MMXII"),
+                "Roman MMXII should equal to 2012");
+    }
+
+    @Test
+    void convertMCMXCVI() {
+        assertEquals(1996, convertRomanToArabicNumber("MCMXCVI"),
+                "Roman MCMXCVI should equal to " +
+                        "1996");
     }
 }
