7cbaf126744f24a567e3400ef9d7ee043bc233c8
Mohamed Taman <mohamed.taman@gmail.com>
2019-04-14 19:19:19 +0200

    Step 5, refactoring (cleaning test class)

--- RomanConverter/src/test/java/rs/com/tm/siriusxi/tdd/roman/RomanConverterTest.java
+++ RomanConverter/src/test/java/rs/com/tm/siriusxi/tdd/roman/RomanConverterTest.java
@@ -24,17 +24,21 @@
 
 package rs.com.tm.siriusxi.tdd.roman;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Test class, to validate the working mechanism of Roman converter class.
+ *
+ * @author mohamed_taman
+ * @since 1.0
  */
 class RomanConverterTest {
 
     @Test
     void convertI() {
-        Assertions.assertEquals(
+        assertEquals(
                 1, new RomanConverter().convertRomanToArabicNumber("I"),
                 "Roman I should equal to 1");
     }
